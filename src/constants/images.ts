239c9087b3314cca2d2bf06a2db233383e34cf49
João Cascais <jpmcascais@gmail.com>
2024-12-04 19:11:19 +0000

    First drafts of the website

---
+++ src/constants/images.ts
@@ -0,0 +1,19 @@
+import adav from '../assets/ADAV.png';
+import ba from '../assets/BA.png';
+import caritas from '../assets/caritas.jpg';
+import cazinha from '../assets/cazinha_icon.png';
+import maozinha from '../assets/maozinha_icon.png';
+import icon_main from '../assets/icon_main.svg';
+import person from '../assets/person_incon.png';
+import retangulo from '../assets/retangulo_cazinha.png'
+
+export default {
+    adav,
+    ba,
+    caritas,
+    cazinha,
+    maozinha,
+    icon_main,
+    person,
+    retangulo
+};
---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,11 @@
+import Home from '~/pages/home/Home';
+
+
+
+export default function Landing() {
+    return (
+      <>
+        <Home />
+      </>
+    )
+  }
